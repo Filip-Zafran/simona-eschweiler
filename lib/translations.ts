d0a2422188e909bdaec9f2d9271c550ae6ce0e0b
Filip Zafran <filip.zafran@gmail.com>
2026-08-11 15:27:32 +0200

    fix navbar

--- lib/translations.ts
+++ lib/translations.ts
@@ -4,14 +4,14 @@ export const t = {
   de: {
     nav: [
       ["Startseite", "/"], ["Leistungen", "/leistungen"], ["Traumasensible Begleitung", "/traumasensible-begleitung"],
-      ["Über mich", "/ueber-mich"], ["Aktuelles", "/aktuelles"], ["Onlineangebote", "/onlineangebote"], ["Kontakt", "/kontakt"]
+      ["Über mich", "/ueber-mich"], ["Onlineangebote", "/onlineangebote"], ["Kontakt", "/kontakt"]
     ],
     enquiry: "Betreuung anfragen", learn: "Mehr erfahren", email: "E-Mail schreiben",
   },
   en: {
     nav: [
       ["Home", "/"], ["Services", "/leistungen"], ["Trauma-sensitive support", "/traumasensible-begleitung"],
-      ["About me", "/ueber-mich"], ["Updates", "/aktuelles"], ["Online services", "/onlineangebote"], ["Contact", "/kontakt"]
+      ["About me", "/ueber-mich"], ["Online services", "/onlineangebote"], ["Contact", "/kontakt"]
     ],
     enquiry: "Enquire about care", learn: "Learn more", email: "Send an email",
   }
